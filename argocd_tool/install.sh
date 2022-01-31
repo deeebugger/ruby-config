e3431bc96b3574c99478f1725b49b373f5baab2e
Ishara Niwarthana <ishara.niwarthana01@gmail.com>
2022-01-31 17:29:41 +0530

    update script to use minikube

--- argocd_tool/install.sh
+++ argocd_tool/install.sh
@@ -1,2 +1,5 @@
+minikube start -p target-k8s
+minikube start -p argocd-k8s
+kubectl config use-context argocd-k8s
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
